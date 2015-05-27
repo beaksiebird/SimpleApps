d66611d12dcfeb97736c38e4c015fbe487e97f4d
Whitney Hogg <whitneylaurenhogg@gmail.com>
2015-05-27 17:11:02 -0400

    Beginning Progress

---
+++ Images/Images/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Images
+//
+//  Created by Whitney Lauren on 5/27/15.
+//  Copyright (c) 2015 Whitney Lauren. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
---
+++ Images/SongCollectionViewController.swift
@@ -0,0 +1,121 @@
+//
+//  SongCollectionViewController.swift
+//  Images
+//
+//  Created by Whitney Lauren on 5/27/15.
+//  Copyright (c) 2015 Whitney Lauren. All rights reserved.
+//
+
+import UIKit
+
+let reuseIdentifier = "songCollectionCell"
+
+class SongCollectionViewController: UICollectionViewController, UISearchBarDelegate, UISearchDisplayDelegate {
+    
+    @IBOutlet weak var searchBarOutlet: UISearchBar!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        searchBarOutlet.delegate = self
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Register cell classes
+        self.collectionView!.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    func getAlbumArtwork() {
+        
+        let API_URL = "https://itunes.apple.com/search?"
+        let searchTerm = "term=\(searchBarOutlet.text)"
+        let parameterKeyValue = "&entity=album"
+        
+        
+        let endpoint = API_URL + searchTerm + parameterKeyValue
+        
+        let url = NSURL(string: endpoint)
+        let request = NSURLRequest(URL: url!)
+        let connection = NSURLConnection(request: request, delegate: self, startImmediately: false)
+    
+        println(endpoint)
+        
+        connection?.start()
+    }
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        getAlbumArtwork()
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        //#warning Incomplete method implementation -- Return the number of sections
+        return 0
+    }
+
+
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        //#warning Incomplete method implementation -- Return the number of items in the section
+        return 0
+    }
+
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as! UICollectionViewCell
+    
+        // Configure the cell
+    
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(collectionView: UICollectionView, shouldHighlightItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(collectionView: UICollectionView, shouldSelectItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(collectionView: UICollectionView, shouldShowMenuForItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(collectionView: UICollectionView, canPerformAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) -> Bool {
+        return false
+    }
+
+    override func collectionView(collectionView: UICollectionView, performAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) {
+    
+    }
+    */
+
+}
