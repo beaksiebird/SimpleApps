b5d63770311016d42b5f305c186d9bc59254ed12
Whitney Hogg <whitneylaurenhogg@gmail.com>
2015-05-27 14:39:13 -0400

    Pod example

---
+++ BeaksieButton/BeaksieButton/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BeaksieButton
+//
+//  Created by Whitney Lauren on 5/27/15.
+//  Copyright (c) 2015 Whitney Lauren. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ BeaksieButton/Pods/Target Support Files/Pods-BeaksieButton-ZFRippleButton/Pods-BeaksieButton-ZFRippleButton-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BeaksieButton_ZFRippleButton : NSObject
+@end
+@implementation PodsDummy_Pods_BeaksieButton_ZFRippleButton
+@end
