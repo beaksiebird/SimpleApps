6e9e901abcb62403864835f43eb71db33b180e81
Whitney Hogg <whitneylaurenhogg@gmail.com>
2015-06-02 17:45:24 -0400

    Progress
    Added images, limit changed to 50, number of comics appears. Also hard
    mode for homework

---
+++ GroupTherapy/GroupTherapy/CharacterTableViewCell.h
@@ -0,0 +1,20 @@
+//
+//  CharacterTableViewCell.h
+//  GroupTherapy
+//
+//  Created by Whitney Lauren on 6/2/15.
+//  Copyright (c) 2015 Whitney Lauren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CharacterTableViewCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *characterImage;
+
+@property (weak, nonatomic) IBOutlet UILabel *cellText;
+
+@property (weak, nonatomic) IBOutlet UILabel *totalComics;
+
+
+@end
--- GroupTherapy/GroupTherapy/CharactersTableViewController.m
+++ GroupTherapy/GroupTherapy/CharactersTableViewController.m
@@ -10,6 +10,8 @@
 
 #import <CommonCrypto/CommonDigest.h>
 
+#import "CharacterTableViewCell.h"
+
 #define API_BASE @"http://gateway.marvel.com/v1/public/"
 
 #define PUBLIC_KEY @"122c80800b73d7bdbcbda6d2796b99cb"
@@ -17,7 +19,7 @@
 #define PRIVATE_KEY @"67e691f9da92edfe3f569b1812741d4183dab9e7"
 
 
-@interface CharactersTableViewController ()
+@interface CharactersTableViewController() <UIScrollViewDelegate>
 
 @end
 
@@ -25,41 +27,17 @@
 
 {
     
-NSArray * characters;
+    NSArray * characters;
 
+    
 }
-
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    int timeStamp = [NSDate date].timeIntervalSince1970;
+    [self characterList];
     
-    NSString * hash = [self MD5String:[NSString stringWithFormat:@"%d%@%@", timeStamp, PRIVATE_KEY, PUBLIC_KEY]];
-    
-    NSString * endpoint = [NSString stringWithFormat: @"%@characters?ts=%d&apikey=%@&hash=%@", API_BASE, timeStamp, PUBLIC_KEY, hash];
-    NSLog(@"%@", endpoint);
-    
-    //gateway.marvel.com/v1/comics?ts=1&apikey=1234&hash=ffd275c5130566a2916217b101f26150
-    NSURL * url = [NSURL URLWithString:endpoint];
-    
-    NSURLRequest * request = [NSURLRequest requestWithURL:url];
-    
-    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
-    
-        NSDictionary * json = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
-        
-        NSLog(@"%@", json);
-        
-        characters = json[@"data"][@"results"];
-        
-        NSLog(@"%@", characters);
-        
-        [self.tableView reloadData];
-        
-    }];
-    
-    
+
     
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
@@ -67,6 +45,56 @@ NSArray * characters;
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
+
+    - (void)scrollViewDidScroll:(UIScrollView *)scrollView{
+        
+        float scrollViewHeight = scrollView.frame.size.height;
+        float scrollContentSizeHeight = scrollView.contentSize.height;
+        float scrollOffset = scrollView.contentOffset.y;
+        
+        if (scrollOffset == 0)
+        {
+            
+        }
+        
+        else if (scrollOffset + scrollViewHeight == scrollContentSizeHeight){
+            
+            [self characterList];
+        }
+        
+    }
+
+- (void)characterList{
+    int timeStamp = [NSDate date].timeIntervalSince1970;
+    
+    NSString * hash = [self MD5String:[NSString stringWithFormat:@"%d%@%@", timeStamp, PRIVATE_KEY, PUBLIC_KEY]];
+    
+    NSString * endpoint = [NSString stringWithFormat: @"%@characters?limit=50&offset=%d&ts=%d&apikey=%@&hash=%@", API_BASE, characters.count, timeStamp, PUBLIC_KEY, hash];
+    NSLog(@"%@", endpoint);
+    
+    NSURL * url = [NSURL URLWithString:endpoint];
+    
+    NSURLRequest * request = [NSURLRequest requestWithURL:url];
+    
+    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+        
+        NSDictionary * json = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+        
+        NSLog(@"%@", json);
+        
+        
+        characters = json[@"data"][@"results"];
+        characters = [characters arrayByAddingObjectsFromArray:characters];
+        
+        
+        NSLog(@"%@", characters);
+        
+        [self.tableView reloadData];
+        
+    }];
+}
+
+
     
 - (NSString *)MD5String: (NSString *)str {
     
@@ -87,8 +115,6 @@ NSArray * characters;
     return output;
 }
     
-    
-    
 
 
 - (void)didReceiveMemoryWarning {
@@ -98,11 +124,12 @@ NSArray * characters;
 
 #pragma mark - Table view data source
 
-//- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-//#warning Potentially incomplete method implementation.
-    // Return the number of sections.
-  //  return 0;
-//}
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+#warning Potentially incomplete method implementation.
+    //Return the number of sections.
+    return 1;
+    
+}
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
 #warning Incomplete method implementation.
@@ -111,13 +138,60 @@ NSArray * characters;
 }
 
 
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"charCell" forIndexPath:indexPath];
+- (CharacterTableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     
-    cell.textLabel.text = characters[indexPath.row][@"name"]; 
+    CharacterTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"charCell" forIndexPath:indexPath];
+    
+    NSDictionary * character = characters[indexPath.row];
+
+    cell.cellText.text = characters[indexPath.row][@"name"];
+    
+    int totalComics = [character[@"comics"][@"available"] intValue];
+    NSString * totalComicsText = [NSString stringWithFormat:@"%i", totalComics];
+    cell.totalComics.text = totalComicsText;
+
+    
+    if (characters[indexPath.row][@"thumbnail"] != nil && characters[indexPath.row][@"thumbnail"] != [NSNull null]) {
+    
+        NSString * path = characters[indexPath.row][@"thumbnail"][@"path"];
+        NSString * extension = characters[indexPath.row][@"thumbnail"][@"extension"];
+        NSString * portraitSmall = @"portrait_small";
+        
+        NSString * completeString = [NSString stringWithFormat: @"%@/%@.%@", path, portraitSmall, extension];
+        NSLog(completeString);
+        
+        NSURL * imageURL = [NSURL URLWithString: completeString];
+        
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
+            
+        NSData * imageData = [NSData dataWithContentsOfURL: imageURL];
+            
+            dispatch_async(dispatch_get_main_queue(), ^{
+                
+                
+                UIImage * cImage = [UIImage imageWithData: imageData];
+                
+                
+                cell.characterImage.image = cImage;
+                
+                
+            });
+            
+        });
+        
+        
+        
+    }
+    
+    cell.totalComics.layer.cornerRadius = 30;
     
     return cell;
 }
+
+
+
+
+
 
 /*
 // Override to support conditional editing of the table view.
