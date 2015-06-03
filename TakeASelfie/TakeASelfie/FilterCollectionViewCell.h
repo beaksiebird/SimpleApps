1ef8880a9cadd5045a4f42d203680c7eac333d8c
Whitney Hogg <whitneylaurenhogg@gmail.com>
2015-06-03 15:26:54 -0400

    Progress

---
+++ TakeASelfie/TakeASelfie/FilterCollectionViewCell.h
@@ -0,0 +1,19 @@
+//
+//  FilterCollectionViewCell.h
+//  TakeASelfie
+//
+//  Created by Whitney Lauren on 6/3/15.
+//  Copyright (c) 2015 Whitney Lauren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FilterCollectionViewCell : UICollectionViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+
+
+
+
+@end
---
+++ TakeASelfie/TakeASelfie/FilterCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  FilterCollectionViewCell.m
+//  TakeASelfie
+//
+//  Created by Whitney Lauren on 6/3/15.
+//  Copyright (c) 2015 Whitney Lauren. All rights reserved.
+//
+
+#import "FilterCollectionViewCell.h"
+
+@implementation FilterCollectionViewCell
+
+@end
