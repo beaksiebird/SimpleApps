09aa147a5c0c35e9ecd1552da9cf18987b14268e
Whitney Hogg <whitneylaurenhogg@gmail.com>
2015-06-30 12:07:52 -0400

    progress

--- Bubbles/Bubbles/BubbleViewController.swift
+++ Bubbles/Bubbles/BubbleViewController.swift
@@ -38,9 +38,49 @@ class BubbleViewController: UIViewController, AVCaptureAudioDataOutputSampleBuff
     
     func captureOutput(captureOutput: AVCaptureOutput!, didOutputSampleBuffer sampleBuffer: CMSampleBuffer!, fromConnection connection: AVCaptureConnection!) {
         
-        println(connection.audioChannels.first)
+        if let audioChannel = connection.audioChannels.first as? AVCaptureAudioChannel {
+            
+            let power = audioChannel.averagePowerLevel + 50
+            
+            if power < 35 {return}
+            
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                
+            
+           //self.view.backgroundColor = UIColor(white: CGFloat(power / 50), alpha: 1.0)
+            
+            var bubble = UIView(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
+                
+                bubble.backgroundColor = UIColor.cyanColor()
+                bubble.layer.cornerRadius = 20
+                
+                bubble.center = CGPoint(x: self.view.center.x, y: self.view.frame.height)
+                
+                self.view.addSubview(bubble)
+                
+                UIView.animateWithDuration(0.2, animations: { () -> Void in
+                    
+                    let randomX = self.getRandomX()
+                    let randomY = self.view.center.y - CGFloat(power - 35) * 10
+                    
+                    bubble.center = CGPoint(x: randomX, y: self.view.center.y)
+                    
+                }, completion: { (finished) -> Void in
+                    
+                    bubble.removeFromSuperview()
+                    
+                    })
+                
+            
+                })
     
+            }
+     
+        }
+    func getRandomX() -> CGFloat {
+        
+        return CGFloat(arc4random_uniform(UInt32(view.frame.width)))
     }
+    
 
 }
-
