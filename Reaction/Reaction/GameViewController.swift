0027214882f8f030246384b6ca121a3158950908
Whitney Hogg <whitneylaurenhogg@gmail.com>
2015-07-01 16:28:46 -0400

    Progress

--- Reaction/Reaction/GameViewController.swift
+++ Reaction/Reaction/GameViewController.swift
@@ -7,19 +7,65 @@
 //
 
 import UIKit
+import GameKit
 
 class GameViewController: UIViewController {
+    
+    var currentScore = 0 {
+        
+        didSet{
+            
+            scoreLabel.text = "\(currentScore)"
+            
+        }
+        
+    }
+    
+    var scoreLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 0, height: 100))
+    
+    var timerBar = UIView(frame: CGRect(x: 0, y: 0, width: 0, height: 10))
     
     var currentCircles: [CircleButton] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        timerBar.backgroundColor = UIColor.whiteColor()
+        view.addSubview(timerBar)
+        
+        scoreLabel.textColor = UIColor.whiteColor()
+        scoreLabel.font = UIFont(name: "MarketFeltThin", size: 80)
+        scoreLabel.text = "0"
+        scoreLabel.frame.origin.y = view.frame.height - 120
+        scoreLabel.frame.size.width = view.frame.width
+        scoreLabel.textAlignment = NSTextAlignment.Center
+        view.addSubview(scoreLabel)
+        
         animateNewCirclesIn()
 
     }
 
+    func runTimer(seconds: NSTimeInterval) {
+        
+        timerBar.layer.removeAllAnimations()
+        
+        timerBar.frame.size.width = view.frame.width
+        
+        UIView.animateWithDuration(seconds, animations: { () -> Void in
+            
+            self.timerBar.frame.size.width = 0
+            
+        }) { (finished) -> Void in
+            
+            if finished { self.endGame() }
+    
+        }
+        
+    }
+    
     func animateNewCirclesIn() {
+        
+        runTimer(5.0)
         
         var cW = (view.frame.width - 120) / 2
         var cR = cW / 2
@@ -31,15 +77,38 @@ class GameViewController: UIViewController {
         for c in 0..<4 {
             
             var circle = CircleButton()
+            circle.choice = c
             circle.frame = CGRect(x: 0, y: 0, width: 0, height: 0)
             circle.center = view.center
             view.addSubview(circle)
+            
+            circle.alpha = c == currentCorrectChoice ? 1.0 : 0.5
+            
+            circle.addTarget(self, action: "tapCircle:", forControlEvents: .TouchUpInside)
             
             currentCircles.append(circle)
             
             let (dx,dy) = directions[c]
             
+            UIView.animateWithDuration(1.0, delay: 0.0, options: UIViewAnimationOptions.AllowUserInteraction, animations: { () -> Void in
+                
+                circle.alpha = 0.5
+                
+                circle.frame.size.width = cW
+                circle.frame.size.height = cW
+                
+                let x = self.view.center.x + dx * (cR + 10)
+                let y = self.view.center.y + dy * (cR + 10)
+                
+                circle.center = CGPoint(x: x, y: y)
+                
+                
+                
+            }, completion: nil)
+            
             UIView.animateWithDuration(1.0, animations: { () -> Void in
+                
+                circle.alpha = 0.5
             
                 circle.frame.size.width = cW
                 circle.frame.size.height = cW
@@ -82,18 +151,45 @@ class GameViewController: UIViewController {
         
     }
     
-    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+    var currentCorrectChoice: Int = Int(arc4random_uniform(4))
+    
+    func tapCircle(circle: CircleButton) {
         
+        if circle.choice == currentCorrectChoice {
+            
+            currentScore++ 
+            
+            //report score to leaderboard
+            
+            let reportScore = GKScore(leaderboardIdentifier: "circles_touched")
+            
+            reportScore.value = Int64(currentScore)
+            
+            GKScore.reportScores([reportScore], withCompletionHandler: { (error) -> Void in
+            
+                println("reported")
+            
+            })
+            
+            //Check if it is the correct button
+            //NEXT
+        
+        currentCorrectChoice = Int(arc4random_uniform(4))
+       
         animateOldCirclesOut()
-        
         currentCircles = []
-        
         animateNewCirclesIn()
+        
+        } else {
+    
+            endGame()
+        }
     }
+
 
     func endGame() {
         
-    if let startVC = storyboard?.instantiateInitialViewController() as? GameViewController {
+    if let startVC = storyboard?.instantiateViewControllerWithIdentifier("startVC") as? ViewController {
                 
         navigationController?.viewControllers = [startVC]
                 
--- Reaction/Reaction/ViewController.swift
+++ Reaction/Reaction/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GameKit
 
 class ViewController: UIViewController {
     
@@ -15,13 +16,62 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
- 
+        
+        GKLocalPlayer.localPlayer().authenticateHandler = {
+            (viewController, error) -> Void in
+            
+            if viewController != nil {
+                
+               self.presentViewController(viewController, animated: true, completion: nil)
+                
+            } else {
+                
+                println(GKLocalPlayer.localPlayer().authenticated)
+                
+                self.loadScore()
+            }
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         
-   
+    }
+    
+        override func viewWillAppear(animated: Bool) {
+            
+            loadScore()
+        }
+        
+    func loadScore() {
+        
+        if GKLocalPlayer.localPlayer().authenticated == false { return }
+        
+        GKLeaderboard.loadLeaderboardsWithCompletionHandler { (leaderboards, error) -> Void in
+            
+            println(leaderboards)
+            
+            for leaderboard in leaderboards as! [GKLeaderboard] {
+                
+                  if leaderboard.identifier == "circles_touched" {
+                    
+                   leaderboard.loadScoresWithCompletionHandler({ (scores, error) -> Void in
+                    
+                    println(scores)
+                    
+                        if leaderboard.localPlayerScore != nil {
+                            
+                            self.topScore.text = "\(leaderboard.localPlayerScore.value)"
+                            
+                        }
+                    
+                    })
+                    
+                }
+            }
+        }
+    
     }
 
 
